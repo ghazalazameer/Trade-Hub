b1c92d1c69bba55551b5430294c08b5d4b9b62cf
ghazalazameer <zameerghazala20@gmail.com>
2024-03-29 02:40:13 +0530

    auth0 setup

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,24 +1,31 @@
 import NextAuth from "next-auth";
-import AppleProvider from "next-auth/providers/apple";
+import TwitterProvider from "next-auth/providers/twitter";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./lib/mongodb";
+import Auth0Provider from "next-auth/providers/auth0";
+
 
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
 
   providers: [
     // OAuth authentication providers...
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER
+    }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
 
-    AppleProvider({
-      clientId: process.env.APPLE_ID,
-      clientSecret: process.env.APPLE_SECRET,
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
@@ -30,7 +37,7 @@ export default NextAuth({
     }),
   ],
   pages: {
-    //signIn: "/signin",
+    signIn: "/signin",
   },
   session: {
     strategy: "jwt",
