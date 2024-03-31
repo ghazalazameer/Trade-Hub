265eceb916429eb20819463b669f38f8506dd98b
ghazalazameer <zameerghazala20@gmail.com>
2024-03-31 18:38:26 +0530

    setting up signin & signup pages

--- pages/signin.js
+++ pages/signin.js
@@ -3,10 +3,61 @@ import Header from "@/components/header";
 import LoginInput from "@/components/inputs/loginInput";
 import styles from "@/styles/signin.module.scss";
 import { Formik, Form } from "formik";
+import * as Yup from "yup";
 import Link from "next/link";
 import { BiLeftArrowAlt } from "react-icons/bi";
+import { useState } from "react";
+import CircledIconBtn from "@/components/buttons/circledIconBtn";
+import { getProviders } from "next-auth/react";
 
-export default function signin() {
+const initialvalues = {
+  login_email: "",
+  login_password: "",
+  name: "",
+  email: "",
+  password: "",
+  conf_password: "",
+};
+export default function Signin({providers}) {
+  console.log(providers);
+  const [user, setUser] = useState(initialvalues);
+    const {
+      login_email,
+      login_password,
+      name,
+      email,
+      password,
+      conf_password,
+    } = user;
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setUser({ ...user, [name]: value });
+    };
+    const loginValidation = Yup.object({
+      login_email: Yup.string().required("Email Address is required!").email("Please enter a valid email address!"),
+      login_password: Yup.string().required("Please enter a Password!"),
+    });
+    const registerValidation = Yup.object({
+      name: Yup.string()
+        .required("What's your name ?")
+        .min(2, "First name must be between 2 and 16 characters.")
+        .max(16, "First name must be between 2 and 16 characters.")
+        .matches(/^[aA-zZ]/, "Numbers and special characters are not allowed."),
+      email: Yup.string()
+        .required(
+          "You'll need this when you log in and if you ever need to reset your password."
+        )
+        .email("Enter a valid email address."),
+      password: Yup.string()
+        .required(
+          "Enter a combination of at least six numbers,letters and punctuation marks(such as ! and &)."
+        )
+        .min(6, "Password must be atleast 6 characters.")
+        .max(36, "Password can't be more than 36 characters"),
+      confirm_password: Yup.string()
+        .required("Confirm your password.")
+        .oneOf([Yup.ref("password")], "Passwords must match."),
+    });
   return (
     <>
       <Header />
@@ -25,13 +76,102 @@ export default function signin() {
             <p>
               Get access to one of the best EShopping services in the world.
             </p>
-            <Formik>
+            <Formik
+            enableReinitialize
+            initialValues={{
+              login_email,
+              login_password,
+            }}
+            validationSchema={loginValidation}
+            >
               {(form) => (
                 <Form>
                   <LoginInput
-                    icon="password"
-                    placeholder="Enter your password"
+                    type="text"
+                    name="login_email"
+                    icon="email"
+                    placeholder="Email Address"
+                    onChange={handleChange}
                   />
+                  <LoginInput
+                    type="password"
+                    name="login_password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                  <CircledIconBtn type="submit" text="Sign in" />
+                  <div className={styles.forget}>
+                    <Link href="/forget">Forget Password ?</Link>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+            <div className={styles.login__socials}>
+              <span className={styles.or}>Or continue with</span>
+              <div className={styles.login__socials_wrap}>
+              {providers.map((provider)=>(
+                <div key={provider.name}>
+                  <button
+                    className={styles.social__btn}
+                    onClick={()=>signIn(provider.id)}
+                    >
+                      <img src={`../../icons/${provider.name}.png`} alt="" />
+                      Sign in with {provider.name}
+                  </button>
+
+                </div>
+              ))}
+              </div>
+            </div>
+          </div>
+        </div><div className={styles.login__container}>
+          <div className={styles.login__form}>
+            <h1>Sign up</h1>
+            <p>
+              Get access to one of the best EShopping services in the world.
+            </p>
+            <Formik
+            enableReinitialize
+            initialValues={{
+              name,
+              email,
+              password,
+              conf_password,
+            }}
+            validationSchema={registerValidation}
+            >
+              {(form) => (
+                <Form>
+                  <LoginInput
+                    type="text"
+                    name="name"
+                    icon="user"
+                    placeholder="Full Name"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="text"
+                    name="email"
+                    icon="email"
+                    placeholder="Email Address"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="conf_password"
+                    icon="password"
+                    placeholder="Re-Type Password"
+                    onChange={handleChange}
+                  />
+                  <CircledIconBtn type="submit" text="Sign up" />
                 </Form>
               )}
             </Formik>
@@ -41,4 +181,11 @@ export default function signin() {
       <Footer country="India" />
     </>
   );
+}
+
+export async function getStaticProps(context){
+  const providers = Object.values(await getProviders());
+  return {
+    props: { providers },
+  };
 }
