c801955ae6b88469fd40ce6ad455a37342c70ecd
ghazalazameer <zameerghazala20@gmail.com>
2024-04-03 03:18:12 +0530

    authentication setup complete

--- pages/signin.js
+++ pages/signin.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import LoginInput from "@/components/inputs/loginInput";
@@ -6,10 +7,14 @@ import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import Link from "next/link";
 import { BiLeftArrowAlt } from "react-icons/bi";
-import { useState } from "react";
 import CircledIconBtn from "@/components/buttons/circledIconBtn";
 import { getProviders } from "next-auth/react";
+import { signIn } from "next-auth/react";
+import Router from "next/router";
+import axios from "axios";
+import DotLoaderSpinner from "@/components/loaders/dotLoader";
 import { set } from "mongoose";
+import { getCsrfToken, getSession } from "next-auth/react";
 
 const initialvalues = {
   login_email: "",
@@ -17,70 +22,103 @@ const initialvalues = {
   name: "",
   email: "",
   password: "",
-  conf_password: "",
+  confirm_password: "",
   success: "",
   error: "",
+  login_error: "",
 };
-export default function Signin({providers}) {
+
+export default function Signin({ providers, callbackUrl, csrfToken }) {
   const [loading, setLoading] = useState(false);
-  console.log(providers);
   const [user, setUser] = useState(initialvalues);
-    const {
-      login_email,
-      login_password,
-      name,
-      email,
-      password,
-      conf_password,
-      success,
-      error,
-    } = user;
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setUser({ ...user, [name]: value });
-    };
-    const loginValidation = Yup.object({
-      login_email: Yup.string().required("Email Address is required!").email("Please enter a valid email address!"),
-      login_password: Yup.string().required("Please enter a Password!"),
-    });
-    const registerValidation = Yup.object({
-      name: Yup.string()
-        .required("What's your name ?")
-        .min(2, "First name must be between 2 and 16 characters.")
-        .max(16, "First name must be between 2 and 16 characters.")
-        .matches(/^[aA-zZ]/, "Numbers and special characters are not allowed."),
-      email: Yup.string()
-        .required(
-          "You'll need this when you log in and if you ever need to reset your password."
-        )
-        .email("Enter a valid email address."),
-      password: Yup.string()
-        .required(
-          "Enter a combination of at least six numbers,letters and punctuation marks(such as ! and &)."
-        )
-        .min(6, "Password must be atleast 6 characters.")
-        .max(36, "Password can't be more than 36 characters"),
-      confirm_password: Yup.string()
-        .required("Confirm your password.")
-        .oneOf([Yup.ref("password")], "Passwords must match."),
-    });
-    const signUpHandler = async () => {
-      try{
-        setLoading(true);
-        const {data}=await axios.post("/api/auth/signup", {
-          name,
-          email,
-          password,
-        });
-        setUser({...user, error: "", success: data.message});
-        setLoading(false);
-      }catch(error){
-        setLoading(false);
-        setUser({...user, sucess: "", error: error.response.data.message});
-      }
+  const {
+    login_email,
+    login_password,
+    name,
+    email,
+    password,
+    confirm_password,
+    success,
+    error,
+    login_error,
+  } = user;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  const loginValidation = Yup.object({
+    login_email: Yup.string()
+      .required("Email Address is required")
+      .email("Please enter a valid email address"),
+    login_password: Yup.string().required("Please enter your password"),
+  });
+  const registerValidation = Yup.object({
+    name: Yup.string()
+      .required("What's your name ?")
+      .min(2, "First name must be between 2 and 16 characters.")
+      .max(16, "First name must be between 2 and 16 characters.")
+      .matches(/^[aA-zZ]/, "Numbers and special characters are not allowed."),
+    email: Yup.string()
+      .required(
+        "You'll need this when you log in and if you ever need to reset your password."
+      )
+      .email("Enter a valid email address."),
+    password: Yup.string()
+      .required(
+        "Enter a combination of at least six numbers,letters and punctuation marks(such as ! and &)."
+      )
+      .min(6, "Password must be atleast 6 characters.")
+      .max(36, "Password can't be more than 36 characters"),
+    confirm_password: Yup.string()
+      .required("Confirm your password.")
+      .oneOf([Yup.ref("password")], "Passwords must match."),
+  });
+
+  const signUpHandler = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post("/api/auth/signup", {
+        name,
+        email,
+        password,
+      });
+      setUser({ ...user, error: "", success: data.message });
+      setLoading(false);
+      setTimeout(async () => {
+        let options = {
+          redirect: false,
+          email: email,
+          password: password,
+        };
+        const res = await signIn("credentials", options);
+        Router.push("/");
+      }, 2000);
+    } catch (error) {
+      setLoading(false);
+      setUser({ ...user, success: "", error: error.response.data.message });
     }
+  };
+
+  const signInHandler = async () => {
+    setLoading(true);
+    let options = {
+      redirect: false,
+      email: login_email,
+      password: login_password,
+    };
+    const res = await signIn("credentials", options);
+    setUser({ ...user, error: "", success: "" });
+    setLoading(false);
+    if (res?.error) {
+      setLoading(false);
+      setUser({ ...user, login_error: res?.error });
+    } else {
+      return Router.push(callbackUrl || "/");
+    }
+  };
   return (
     <>
+      {loading && <DotLoaderSpinner Loading={loading} />}
       <Header />
       <div className={styles.login}>
         <div className={styles.login__container}>
@@ -98,15 +136,17 @@ export default function Signin({providers}) {
               Get access to one of the best EShopping services in the world.
             </p>
             <Formik
-            enableReinitialize
-            initialValues={{
-              login_email,
-              login_password,
-            }}
-            validationSchema={loginValidation}
+              enableReinitialize
+              initialValues={{ login_email, login_password }}
+              validationSchema={loginValidation}
+              onSubmit={() => {
+                signInHandler();
+              }}
             >
               {(form) => (
-                <Form>
+                <Form method="post" action="/api/auth/signin/email">
+                  <input type="hidden" name="csrfToken" defaultValue={csrfToken} />
+
                   <LoginInput
                     type="text"
                     name="login_email"
@@ -122,48 +162,52 @@ export default function Signin({providers}) {
                     onChange={handleChange}
                   />
                   <CircledIconBtn type="submit" text="Sign in" />
-                  <div className={styles.forget}>
-                    <Link href="/forget">Forget Password ?</Link>
+                  {login_error && (
+                    <span className={styles.error}>{login_error}</span>
+                  )}
+                  <div className={styles.forgot}>
+                    <Link href="/forget">Forgot Password ?</Link>
                   </div>
                 </Form>
               )}
             </Formik>
             <div className={styles.login__socials}>
-              <span className={styles.or}>Or continue with</span>
+              <span className={styles.or}>Or continue with </span>
               <div className={styles.login__socials_wrap}>
-              {providers.map((provider)=>(
-                <div key={provider.name}>
-                  <button
-                    className={styles.social__btn}
-                    onClick={()=>signIn(provider.id)}
-                    >
-                      <img src={`../../icons/${provider.name}.png`} alt="" />
-                      Sign in with {provider.name}
-                  </button>
-
-                </div>
-              ))}
+                {providers.map((provider) => {
+                  if (provider.name == "Credentials") {
+                    return;
+                  }
+                  return (
+                    <div key={provider.name}>
+                      <button
+                        className={styles.social__btn}
+                        onClick={() => signIn(provider.id)}
+                      >
+                        <img src={`../../icons/${provider.name}.png`} alt="" />
+                        Sign in with {provider.name}
+                      </button>
+                    </div>
+                  );
+                })}
+                ;
               </div>
             </div>
           </div>
-        </div><div className={styles.login__container}>
+        </div>
+        <div className={styles.login__container}>
           <div className={styles.login__form}>
             <h1>Sign up</h1>
             <p>
               Get access to one of the best EShopping services in the world.
             </p>
             <Formik
-            enableReinitialize
-            initialValues={{
-              name,
-              email,
-              password,
-              conf_password,
-            }}
-            validationSchema={registerValidation}
-            onSubmit={()=>{
-              signUpHandler();
-            }}
+              enableReinitialize
+              initialValues={{ name, email, password, confirm_password }}
+              validationSchema={registerValidation}
+              onSubmit={() => {
+                signUpHandler();
+              }}
             >
               {(form) => (
                 <Form>
@@ -190,7 +234,7 @@ export default function Signin({providers}) {
                   />
                   <LoginInput
                     type="password"
-                    name="conf_password"
+                    name="confirm_password"
                     icon="password"
                     placeholder="Re-Type Password"
                     onChange={handleChange}
@@ -200,15 +244,9 @@ export default function Signin({providers}) {
               )}
             </Formik>
             <div>
-              {
-                success && <span>{success}</span>
-              }
+              {success && <span className={styles.success}>{success}</span>}
             </div>
-            <div>
-              {
-                error && <span>{error}</span>
-              }
-            </div>
+            <div>{error && <span className={styles.error}>{error}</span>}</div>
           </div>
         </div>
       </div>
@@ -217,9 +255,20 @@ export default function Signin({providers}) {
   );
 }
 
-export async function getStaticProps(context){
+export async function getServerSideProps(context) {
+  const { req, query } = context;
+  const session = await getSession({ req });
+  const { callbackUrl } = query;
+  if (session) {
+    return {
+      redirect: {
+        destination: callbackUrl,
+      },
+    };
+  }
+  const csrfToken = await getCsrfToken(context);
   const providers = Object.values(await getProviders());
   return {
-    props: { providers },
+    props: { providers, csrfToken, callbackUrl },
   };
 }
