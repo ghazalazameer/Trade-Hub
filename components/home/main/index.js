9b8ebcf0460f3ce193751623d7085ab12c7347e9
ghazalazameer <zameerghazala20@gmail.com>
2024-04-07 02:09:31 +0530

    edit the main header section

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,15 +1,20 @@
 import styles from "./styles.module.scss";
 import MainSwiper from "./swiper";
 import Offer from "./offers";
+import Menu from "./Menu";
+import User from "./User";
+import { useSession } from "next-auth/react";
+import Header from "./Header";
 
 export default function Main() {
+  const { data: session }= useSession();
   return (
     <div className={styles.main}>
-        <div className={styles.header}>header</div>
-        <div className={styles.menu}>menu</div>
+        <Header/>
+        <Menu />
         <MainSwiper />
         <Offer />
-        <div className={styles.user}>user</div>
+        <User />
     </div>
   )
 }
