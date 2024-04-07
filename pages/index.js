495cc5a42cc457c200ad8be21e2fa5f10c5d5921
ghazalazameer <zameerghazala20@gmail.com>
2024-04-08 02:39:51 +0530

    updated with home components

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,51 @@ import Image from "next/image";
 import styles from "@/styles/Home.module.scss";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import axios from "axios";
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
 import Main from "@/components/home/main";
+import FlashDeals from "@/components/home/flashDeals";
+import Category from "@/components/home/category";
+import { BsArrowRight } from "react-icons/bs";
+import { women_accessories, women_dresses, women_shoes } from "@/data/home";
+import { useMediaQuery } from "react-responsive";
 
 export default function Home() {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
+  const isMedium = useMediaQuery({ query: "(max-width:850px)" });
+  const isMobile = useMediaQuery({ query: "(max-width:550px)" });
   return (
     <>
       <Header />
       <div className={styles.home}>
         <div className={styles.container}>
           <Main />
+          <FlashDeals />
+          <div className={styles.home__category}>
+            <Category
+              header="Dresses"
+              products={women_dresses}
+              background="#5a31f4"
+            />
+            {!isMedium && (
+              <Category
+                header="Shoes"
+                products={women_shoes}
+                background="#3c811f"
+              />
+            )}
+            {isMobile && (
+              <Category
+                header="Shoes"
+                products={women_shoes}
+                background="#3c811f"
+              />
+            )}
+            <Category
+              header="Accessories"
+              products={women_accessories}
+              background="#000"
+            />
+          </div>
         </div>
       </div>
       <Footer />
