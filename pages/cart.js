379e0b79d8295fdc80fd02c3704c8aaa1a236b61
ghazalazameer <zameerghazala20@gmail.com>
2024-04-21 02:17:20 +0530

    continue 'add to cart' with more func. (got bug)

--- pages/cart.js
+++ pages/cart.js
@@ -1,30 +1,98 @@
-import {useSelector} from "react-redux"; 
+import { useSelector, useDispatch } from "react-redux";
 import Header from "@/components/cart/header";
 import styles from "../styles/cart.module.scss";
-import Empty from "@/components/cart/empty"; 
+import Empty from "@/components/cart/empty";
 import Product from "@/components/cart/product";
+import { useEffect, useState } from "react";
+import { updateCart } from "../store/cartSlice";
+import CartHeader from "@/components/cart/cartHeader";
+import Checkout from "@/components/cart/checkout";
+import PaymentMethods from "@/components/cart/paymentMethods";
+import ProductsSwiper from "@/components/productsSwiper";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { saveCart } from "../requests/user";
+import { women_swiper } from "../data/home";
+import axios from "axios";
 
 export default function Cart() {
-    const { cart } = useSelector((state) => ({ ...state }));
-return (
+    const Router = useRouter();
+    const {data:session}=useSession(); 
+  const [selected, setSelected] = useState([]);
+  const { cart } = useSelector((state) => ({ ...state }));
+  const dispatch = useDispatch();
+
+  // ------
+  const [shippingFee, setShippingFee] = useState(0);
+  const [subtotal, setSubtotal] = useState(0);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    const update = async () => {
+      const { data } = await axios.post("/api/updateCart", {
+        products: cart.cartItems,
+      });
+      dispatch(updateCart(data));
+    };
+    if (cart && cart.cartItems && cart.cartItems.length > 0) {
+      update();
+    }
+  }, [cart, dispatch]);
+  useEffect(() => {
+    setShippingFee(
+      selected.reduce((a, c) => a + Number(c.shipping), 0).toFixed(2)
+    );
+    setSubtotal(selected.reduce((a, c) => a + c.price * c.qty, 0).toFixed(2));
+    setTotal(
+      (
+        selected.reduce((a, c) => a + c.price * c.qty, 0) + Number(shippingFee)
+      ).toFixed(2)
+    );
+  }, [selected]);
+  // ------
+
+  const saveCartToDbHandler = async () => {
+    if (session) {
+      const res = saveCart(selected, session.user.id);
+      Router.push("/checkout");
+    } else {
+      signIn();
+    }
+  };
+  return (
     <>
-        <Header />
-        <div className={styles.cart}>
-        {cart && cart.cartItems && cart.cartItems.length > 0 ? (
-            <div className={styles.cart__container}>
-                <div className={styles.cart__product}>
-                    {
-                        cart.cartItems.map((product)=>(
-                            <Product product={product} key={product._uid} />
-                        ))
-                    }
-                </div>
+      <Header />
+      <div className={styles.cart}>
+      {cart && cart.cartItems && cart.cartItems.length > 0 ? (
+          <div className={styles.cart__container}>
+            <CartHeader
+              cartItems={cart.cartItems}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <div className={styles.cart__product}>
+              {cart.cartItems.map((product) => (
+                <Product
+                  product={product}
+                  key={product._uid}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+              ))}
             </div>
-            ) : (
-                 <Empty />
-            )
-            }
-        </div>
+            <Checkout
+              subtotal={subtotal}
+              shippingFee={shippingFee}
+              total={total}
+              selected={selected}
+              saveCartToDbHandler={saveCartToDbHandler}
+            />
+            <PaymentMethods />
+          </div>
+        ) : (
+          <Empty />
+        )}
+        <ProductsSwiper products={women_swiper} />
+      </div>
     </>
-);
+  );
 }
