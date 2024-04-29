d30287e783d94a2e75e6ea8577359197eb2e4cec
ghazalazameer <zameerghazala20@gmail.com>
2024-04-30 01:20:44 +0530

    worked on admin dashboard sidebar & setup toastify

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import Head from "next/head";
 import { SessionProvider } from "next-auth/react"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 let persistor = persistStore(store);
 
@@ -20,6 +22,18 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
     <SessionProvider session={session}>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="colored"
+/>
         <Component {...pageProps} />
       </PersistGate>
     </Provider>
--- pages/admin/dashboard/index.js
+++ pages/admin/dashboard/index.js
@@ -1,12 +1,17 @@
 import Layout from "@/components/admin/layout";
-import styles from "../../styles/dashboard.module.scss";
+import styles from "../../../styles/dashboard.module.scss";
+import { toast } from 'react-toastify';
 
-export default function index() {
+export default function dashboard() {
   return (
     <div>
       <Layout>
-        
+        <button onClick={()=>toast.success("Everything is working fine !!", {
+          position: "top-right",
+        })}>
+          Click to show toastify
+        </button>
       </Layout>
     </div>
-  )
+  );
 }
