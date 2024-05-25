e4cf09ab1c4be659101f1ba99fdb40da4d17f005
ghazalazameer <zameerghazala20@gmail.com>
2024-05-25 23:10:58 +0530

    fixed the discount bug

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -69,28 +69,32 @@ export async function getServerSideProps(context) {
       return a - b;
     });
 
-  let newProduct = {
-    ...product,
-    style,
-    images: subProduct.images,
-    sizes: subProduct.sizes,
-    discount: subProduct.discount,
-    sku: subProduct.sku,
-    colors: product.subProducts.map((p) => {
-      return p.color;
-    }),
-    priceRange: subProduct.discount
-      ? `From ${(prices[0] * (1 - subProduct.discount)).toFixed(2)} to ${(
-          prices[prices.length - 1] * (1 - subProduct.discount)
-        ).toFixed(2)}$`
-      : `From ${prices[0]} to ${prices[prices.length - 1]}$`,
-    price:
-      subProduct.discount > 0
-        ? (subProduct.sizes[size].price * (1 - subProduct.discount)).toFixed(2)
-        : subProduct.sizes[size].price,
-    priceBefore: subProduct.sizes[size].price,
-    quantity: subProduct.sizes[size].qty,
-    ratings: [
+    let newProduct = {
+      ...product,
+      style,
+      images: subProduct.images,
+      sizes: subProduct.sizes,
+      discount: subProduct.discount,
+      sku: subProduct.sku,
+      colors: product.subProducts.map((p) => {
+        return p.color;
+      }),
+      priceRange: subProduct.discount
+        ? `From ${(prices[0] - prices[0] / subProduct.discount).toFixed(2)} to ${(
+            prices[prices.length - 1] -
+            prices[prices.length - 1] / subProduct.discount
+          ).toFixed(2)}₹`
+        : `From ${prices[0]} to ${prices[prices.length - 1]}₹`,
+      price:
+        subProduct.discount > 0
+          ? (
+              subProduct.sizes[size].price -
+              subProduct.sizes[size].price / subProduct.discount
+            ).toFixed(2)
+          : subProduct.sizes[size].price,
+      priceBefore: subProduct.sizes[size].price,
+      quantity: subProduct.sizes[size].qty,
+      ratings: [
       {
         percentage: calculatePercentage("5"),
       },
